6fb680955adc8b1d561a0490cae652448fcfa87f
Rafal Lechowicz <lechowicz.rafal@gmail.com>
2017-02-15 16:50:14 +0100

    first commit

---
+++ IdeaProjects/shop/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Rafał on 2017-02-12.
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Main extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("Hello World");
+    }
+}
