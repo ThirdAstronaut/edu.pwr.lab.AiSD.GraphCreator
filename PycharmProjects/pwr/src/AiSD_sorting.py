080c080d847aca8866960de7a221fc6734b3db06
Rafal Lechowicz <lechowicz.rafal@gmail.com>
2017-04-20 13:19:00 +0200

    ok

---
+++ PycharmProjects/pwr/src/AiSD_sorting.py
@@ -0,0 +1,119 @@
+from matplotlib import animation
+from pylab import *
+
+##file = open('C:/Users/Rafał/IdeaProjects/SortingAlghoritms_AiSD/results.txt')
+"""
+with open('C:/Users/Rafał/IdeaProjects/SortingAlghoritms_AiSD/test.txt') as f:
+
+    array = [[int(x) for x in line.split("/")] for line in f]
+title("sorting alghoritms")
+plot(1, 2, 3, 4, 'r', label="shell sort")
+legend(loc='upper right')
+savefig('graph1.png')
+
+"""
+
+theFile = open("C:/Users/Rafał/IdeaProjects/SortingAlghoritms_AiSD/values.txt", "r")
+theInts = []
+for val in theFile.read().split("/"):
+    theInts.append(int(val))
+theFile.close()
+
+
+title("sorting alghoritms")
+plot(theInts, 'r', label="shell sort")
+nums = [1,2,4,8,16,32]
+rands = [1,5,12,25,16,3,24,11,14]
+plot( nums,'b', label="library sort")
+plot(rands, 'g', label="quick sort")
+legend(loc='upper right')
+
+
+savefig('graph1.png')
+
+
+
+
+
+
+
+"""
+
+
+values = []
+for line in file:
+    fields = line.strip().split("/")
+
+    #for nums in range(len(fields)):
+  #      values = fields[nums]
+    title("sorting alghoritms")
+    plot(1,2,3,4,'r', label = "shell sort")
+    legend(loc='upper right')
+    savefig('graph1.png')
+"""
+
+
+""" 'r' = red
+ 'g' = green
+ 'b' = blue
+ 'c' = cyan
+ 'm' = magenta
+ 'y' = yellow
+ 'k' = black
+ 'w' = white
+
+  '-' = solid
+ '--' = dashed
+ ':' = dotted
+ '-.' = dot-dashed
+ '.' = points
+ 'o' = filled circles
+ '^' = filled triangles"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+    text_file = open("filename.dat", "r")
+
+    print
+    lines
+    print
+    len(lines)
+    text_file.close()
+
+
+
+
+
+
+    """
+
+"""
+    filename = "C:/Users/Rafał/IdeaProjects/SortingAlghoritms_AiSD/results.txt"
+    values = []
+    with open(filename) as f:
+        for line in f:
+            values.append([int(n) for n in line.sptrip().split("/")])
+            for num in values:
+            plot([values[num]])
+           """
